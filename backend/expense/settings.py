8657625d9a204eb26e1cb35c1942f2fa3cd4a576
Subomi Obans <subomi.obans3@gmail.com>
2025-09-08 11:39:29 +0100

    testing my authentication out for bugs

--- backend/expense/settings.py
+++ backend/expense/settings.py
@@ -147,8 +147,8 @@ REST_FRAMEWORK = {
 
 
 SIMPLE_JWT = {
-    "ACCESS_TOKEN_LIFETIME": timedelta(minutes=5),
-    "REFRESH_TOKEN_LIFETIME": timedelta(days=1),
+    "ACCESS_TOKEN_LIFETIME": timedelta(seconds=30),
+    "REFRESH_TOKEN_LIFETIME": timedelta(seconds=35),
     "ROTATE_REFRESH_TOKENS": True,
     "BLACKLIST_AFTER_ROTATION": True,
 }
