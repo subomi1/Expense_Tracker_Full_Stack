5bfd55ff922f59241e3f6b01724e3b6411b2a380
Subomi Obans <subomi.obans3@gmail.com>
2025-09-08 11:42:11 +0100

    added some new classes for styling

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -9,14 +9,20 @@ const Navbar = ({ name }) => {
     firstLetter = user[0];
   }
   return (
-    <nav className="w-full bg-white shadow py-3 px-8 flex justify-between items-center mb-10">
-      <h1 className="text-[#6366f1] text-2xl font-bold">{name}</h1>
+    <nav className="w-full bg-[#F5F3F0] shadow-md py-3 px-8 flex justify-between items-center mb-10 border-b-2 border-b-[#A8A2A3]">
+      <h1 className="text-[#3D352E] text-2xl font-bold">{name}</h1>
       <div className="flex items-center gap-3">
-        {user ? (        <div className="w-[50px] h-[50px] bg-[#6366f1] rounded-full flex justify-center items-center text-4xl">
-          <span className="mt-[-9px] text-white">{firstLetter}</span>
-        </div>) : (        <div className="w-[50px] h-[50px] bg-[#6366f1] rounded-full flex justify-center items-center text-4xl">
-          <span className=" text-white"><User/></span>
-        </div>)}
+        {user ? (
+          <div className="w-[50px] h-[50px] bg-[#6366f1] rounded-full flex justify-center items-center text-4xl">
+            <span className="mt-[-9px] text-white">{firstLetter}</span>
+          </div>
+        ) : (
+          <div className="w-[50px] h-[50px] bg-[#6366f1] rounded-full flex justify-center items-center text-4xl">
+            <span className=" text-white">
+              <User />
+            </span>
+          </div>
+        )}
       </div>
     </nav>
   );
--- frontend/src/components/Sidebar.jsx
+++ frontend/src/components/Sidebar.jsx
@@ -9,21 +9,35 @@ import {
 import { NavLink } from "react-router-dom";
 import { AuthContext } from "../store/AuthContext";
 import { useContext } from "react";
+import { useMutation } from "@tanstack/react-query";
+import { queryClient } from "../http";
+import { useNavigate } from "react-router-dom";
 const Sidebar = () => {
   const { user, email, logout } = useContext(AuthContext);
+  const navigate = useNavigate()
+  const { mutate } = useMutation({
+    mutationFn: logout,
+    onSuccess: () => {
+      queryClient.invalidateQueries({ queryKey: ["category"] });
+      navigate('/dashboard')
+    }
+  })
+  function handleLogout() {
+    mutate();
+  }
   let linkClass =
-    "flex gap-1 items-center text-[#71717a] font-bold mb-4 py-2 px-2";
+    "flex gap-1 items-center text-[#CDAF94] font-bold mb-4 py-2 px-2";
   let activeClass =
-    "flex gap-1 items-center text-[#71717a] font-bold mb-4 border-b-2 border-[#6366f1] rounded-b-md py-2 px-2";
+    "flex gap-1 items-center text-[#CDAF94] font-bold mb-4 border-b-2 border-[#7A4433] rounded-b-md py-2 px-2";
   let firstLetter = "";
   if (user) {
     firstLetter = user[0]
   }
   return (
-    <aside className="w-[250px] bg-white h-screen rounded-tr-2xl shadow-2xl py-10 flex flex-col border-r-1 border-r-black  max-md:hidden justify-between">
+    <aside className="w-[250px] bg-[#3D352E] min-h-screen shadow-2xl py-10 flex flex-col border-r-1 border-r-[#8F8C8A]  max-md:hidden justify-between">
       <div className="flex flex-col items-center">
         <img src={expense} alt="Expense Icon" className="w-20 " />
-        <h1 className=" mb-10 font-bold text-xl">Expense Tracker</h1>
+        <h1 className=" mb-10 font-bold text-xl text-[#CDAF94]">Expense Tracker</h1>
         <nav>
           <NavLink
             className={({ isActive }) => (isActive ? activeClass : linkClass)}
@@ -50,7 +64,7 @@ const Sidebar = () => {
       </div>
       <div className="flex flex-col items-center">
         {user ? (
-          <button className={`${linkClass} cursor-pointer`} onClick={logout}>
+          <button className={`${linkClass} cursor-pointer`} onClick={handleLogout}>
             <LogOut />
             Logout
           </button>
@@ -63,11 +77,11 @@ const Sidebar = () => {
             Login
           </NavLink>
         )}
-        {user ? (        <div className="flex items-center w-full justify-center gap-3">
+        {user ? (        <div className="flex items-center w-full justify-center gap-3 flex-wrap flex-col">
           <div className="w-[50px] h-[50px] bg-[#6366f1] rounded-full flex justify-center items-center text-4xl">
             <span className="mt-[-9px] text-white">{firstLetter}</span>
           </div>
-          <div>
+          <div className="text-center flex flex-col">
             <p>{user}</p>
             <p>{email}</p>
           </div>
