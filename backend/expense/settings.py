4741705d2a7928239bf91f2ca976e406067fb247
Subomi Obans <subomi.obans3@gmail.com>
2025-09-24 11:22:40 +0100

    added some config to set my new income app

--- backend/expense/settings.py
+++ backend/expense/settings.py
@@ -44,7 +44,8 @@ INSTALLED_APPS = [
     'rest_framework_simplejwt',
     'rest_framework_simplejwt.token_blacklist',
     "corsheaders",
-    "Inexpenses.apps.InexpensesConfig"
+    "Inexpenses.apps.InexpensesConfig",
+    "income.apps.IncomeConfig"
 ]
 
 MIDDLEWARE = [
